d4fc23ef688559797b78d7bce6616d47f441a9be
Morad-eh <morad-eh@outlook.fr>
2026-08-21 18:04:22 +0200

    config : enregistrement context processor + toutes les nouvelles routes

--- sportify/settings.py
+++ sportify/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.i18n',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'gestion.context_processors.admin_alertes',
             ],
         },
     },
